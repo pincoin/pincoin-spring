3de22f9dbc3eba9a0e8d215b5a325812fccd51e0
pincoin <pincoins@gmail.com>
2023-02-04 20:20:25 +0900

    doFilterInternal optional

--- src/main/java/kr/pincoin/be/member/jwt/JwtFilter.java
+++ src/main/java/kr/pincoin/be/member/jwt/JwtFilter.java
@@ -37,55 +37,36 @@ public class JwtFilter extends OncePerRequestFilter {
                                     FilterChain chain) throws
                                                        ServletException,
                                                        IOException {
-
         // 1. 헤더에서 액세스 토큰 가져오기
-        final Optional<String> token = Optional.ofNullable(tokenProvider.getBearerToken(request));
+        Optional.ofNullable(tokenProvider.getBearerToken(request))
+                // 2. 액세스 토큰 파싱 유효성 검증
+                .flatMap(tokenProvider::validateAccessToken)
+                .ifPresent(username -> {
+                    try {
+                        // 3. 사용자 디비 조회
+                        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-        if (token.isEmpty()) {
-            // 토큰 포맷, 만료시각, 서명 유효성 검사 실패
-            chain.doFilter(request, response);
-            return;
-        }
+                        // 4. 컨텍스트에 사용자 인증 처리
+                        // AuthenticationManager 또는 AuthenticationProvider 구현체에서 isAuthenticated() = true
+                        // 인증 토큰을 통과한 경우에만 이 생성자로 authentication 객체를 만든다.
+                        UsernamePasswordAuthenticationToken authentication =
+                                new UsernamePasswordAuthenticationToken(
+                                        userDetails, // principal: userDetails
+                                        null, // credentials
+                                        List.of() // userDetails.getAuthorities()
+                                );
 
-        // 2. 액세스 토큰 파싱 유효성 검증
-        final Optional<String> username = tokenProvider.validateAccessToken(token.get());
+                        // 인증부가기능(WebAuthenticationDetails, WebAuthenticationDetailsSource) 저장
+                        // authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-        if (username.isEmpty()) {
-            // 토큰 포맷, 만료시각, 서명 유효성 검사 실패
-            chain.doFilter(request, response);
-            return;
-        }
-
-        // 3. 사용자 디비 조회
-        UserDetails userDetails;
-
-        try {
-            userDetails = userDetailsService.loadUserByUsername(username.get());
-        } catch (UsernameNotFoundException ignored) {
-            log.warn("{} is not found.", username.get());
-            chain.doFilter(request, response);
-            return;
-        }
-
-        // 4. 컨텍스트에 사용자 인증 처리
-        // AuthenticationManager 또는 AuthenticationProvider 구현체에서 isAuthenticated() = true
-        // 인증 토큰을 통과한 경우에만 이 생성자로 authentication 객체를 만든다.
-        UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(userDetails,
-                                                        // principal: userDetails
-                                                        null, List.of()
-                                                        // userDetails.getAuthorities()
-                );
-
-        // 인증부가기능(WebAuthenticationDetails, WebAuthenticationDetailsSource) 저장
-        // authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-        // 컨텍스트에 인증을 설정 한 후 현재 사용자가 인증되도록 지정한다.
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+                        // 컨텍스트에 인증을 설정 한 후 현재 사용자가 인증되도록 지정한다.
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    } catch (UsernameNotFoundException ignored) {
+                        log.warn("{} is not found.", username);
+                    }
+                });
 
         // 5. 이후 필터 수행
-        // 스프링 시큐리티 필터들이 모두 정해진 순서대로 FilterChain에 엮인다.
-        // 각자 로직을 수행하고 다음 필터 클래스의 doFilter() 메소드를 실행시킨다.
         chain.doFilter(request, response);
     }
 }
