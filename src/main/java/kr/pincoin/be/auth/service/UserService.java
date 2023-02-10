ffbd676c5d86c0c776b1950f65de3a14e4325940
pincoin <pincoins@gmail.com>
2023-02-11 01:09:11 +0900

    db refresh token

--- src/main/java/kr/pincoin/be/auth/service/UserService.java
+++ src/main/java/kr/pincoin/be/auth/service/UserService.java
@@ -9,15 +9,16 @@ import kr.pincoin.be.auth.repository.UserRepository;
 import kr.pincoin.be.home.dto.AccessTokenResponse;
 import kr.pincoin.be.home.dto.PasswordGrantRequest;
 import kr.pincoin.be.home.dto.RefreshTokenRequest;
-import kr.pincoin.be.member.domain.RedisRefreshToken;
+import kr.pincoin.be.member.domain.DbRefreshToken;
+import kr.pincoin.be.member.repository.DbRefreshTokenRepository;
 import kr.pincoin.be.member.repository.ProfileRepository;
-import kr.pincoin.be.member.repository.RedisRefreshTokenRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,19 +30,21 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final ProfileRepository profileRepository;
-    private final RedisRefreshTokenRepository redisRefreshTokenRepository;
+
+
+    private final DbRefreshTokenRepository dbRefreshTokenRepository;
 
     private final TokenProvider tokenProvider;
     private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository,
                        ProfileRepository profileRepository,
-                       RedisRefreshTokenRepository redisRefreshTokenRepository,
+                       DbRefreshTokenRepository dbRefreshTokenRepository,
                        TokenProvider tokenProvider,
                        PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.profileRepository = profileRepository;
-        this.redisRefreshTokenRepository = redisRefreshTokenRepository;
+        this.dbRefreshTokenRepository = dbRefreshTokenRepository;
         this.tokenProvider = tokenProvider;
         this.passwordEncoder = passwordEncoder;
     }
@@ -63,7 +66,7 @@ public class UserService {
     @Transactional
     public Optional<AccessTokenResponse>
     refresh(RefreshTokenRequest request) {
-        return redisRefreshTokenRepository.findByRefreshToken(request.getRefreshToken())
+        return userRepository.findActiveUserWithDbRefreshToken(request.getRefreshToken(), LocalDateTime.now())
                 .map(this::getAccessTokenResponse);
     }
 
@@ -96,8 +99,8 @@ public class UserService {
                                                  request.getLastName())
                                                 .activate());
 
-        // 2. 리프레시 토큰 저장을 위한 빈 레코드 추가 (디비 저장 시)
-        // refreshTokenRepository.save(new DbRefreshToken(user));
+        // 2. 리프레시 토큰 디비 저장을 위한 빈 레코드 추가
+        dbRefreshTokenRepository.save(new DbRefreshToken(user));
 
         return new UserResponse(user.getUsername(),
                                 user.getFirstName(),
@@ -135,32 +138,16 @@ public class UserService {
     }
 
     private AccessTokenResponse getAccessTokenResponse(User user) {
-        // 1. 기존 리프레시 토큰 모두 삭제
-        redisRefreshTokenRepository.deleteAll(redisRefreshTokenRepository.findByUserId(user.getId()));
-
-        // 2. 액세스 토큰 생성 (디비 저장 안 함)
+        // 1. 액세스 토큰 생성 (디비 저장 안 함)
         String accessToken = tokenProvider.createAccessToken(user.getUsername(), user.getId());
 
-        // 3. 리프레시 토큰 생성 (디비 저장)
+        // 2. 리프레시 토큰 생성 (디비 저장)
         String refreshToken = tokenProvider.createRefreshToken();
-        // dbRefreshTokenRepository.save(new DbRefreshToken(user).issueRefreshToken(refreshToken));
-        redisRefreshTokenRepository.save(new RedisRefreshToken(user.getId(), user.getUsername(), user.isActive())
-                        .issueRefreshToken(refreshToken));
 
-        return new AccessTokenResponse(accessToken, ACCESS_TOKEN_EXPIRES_IN, refreshToken);
-    }
+        DbRefreshToken refreshTokenFound = dbRefreshTokenRepository.findByUser(user)
+                .orElseThrow(() -> new RuntimeException("Failed to issue refresh token"));
 
-    private AccessTokenResponse getAccessTokenResponse(RedisRefreshToken token) {
-        // 1. 기존 리프레시 토큰 모두 삭제
-        redisRefreshTokenRepository.deleteAll(redisRefreshTokenRepository.findByUserId(token.getUserId()));
-
-        // 2. 액세스 토큰 생성 (디비 저장 안 함)
-        String accessToken = tokenProvider.createAccessToken(token.getUsername(), token.getId());
-
-        // 3. 리프레시 토큰 생성 (Redis 저장)
-        String refreshToken = tokenProvider.createRefreshToken();
-        redisRefreshTokenRepository.save(new RedisRefreshToken(token.getUserId(), token.getUsername(), token.isActive())
-                                                 .issueRefreshToken(refreshToken));
+        dbRefreshTokenRepository.save(refreshTokenFound.issueRefreshToken(refreshToken));
 
         return new AccessTokenResponse(accessToken, ACCESS_TOKEN_EXPIRES_IN, refreshToken);
     }
--- src/main/java/kr/pincoin/be/member/domain/RedisRefreshToken.java
+++ src/main/java/kr/pincoin/be/member/domain/RedisRefreshToken.java
@@ -3,6 +3,7 @@ package kr.pincoin.be.member.domain;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
 import java.time.Duration;
@@ -13,23 +14,25 @@ import static kr.pincoin.be.auth.jwt.TokenProvider.REFRESH_TOKEN_EXPIRES_IN;
 
 
 @Getter
-@RedisHash(value = "refresh_token")
+@RedisHash(value = "refresh_token", timeToLive = 5)
 public class RedisRefreshToken {
     @Id
     private Long id;
     @Indexed // 검색 가능 색인 필드
     private String refreshToken;
     private LocalDateTime expiresIn;
+    @TimeToLive
+    private final Long ttl;
     @Indexed
     private final Long userId;
-    @Indexed // 검색 가능 색인 필드
     private final String username;
     private final boolean active;
 
-    public RedisRefreshToken(Long userId, String username, boolean active) {
+    public RedisRefreshToken(Long userId, String username, boolean active, Long ttl) {
         this.userId = userId;
         this.username = username;
         this.active = active;
+        this.ttl = ttl;
     }
 
     public RedisRefreshToken issueRefreshToken(String refreshToken) {
