a8c059863b8e5b6df4bae15fc44e304d983e3eb8
pincoin <pincoins@gmail.com>
2023-02-12 01:09:51 +0900

    rest controller advice

--- src/main/java/kr/pincoin/be/home/exception/ApiExceptionHandler.java
+++ src/main/java/kr/pincoin/be/home/exception/ApiExceptionHandler.java
@@ -7,13 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class ApiExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
